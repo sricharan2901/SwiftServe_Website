e99efd39a8f136960ab211795d020407f1b94088
Sricharan Sridhar <140080879+sricharan2901@users.noreply.github.com>
2024-04-22 22:32:07 +0530

    updated products.js

---
+++ src/products.js
@@ -0,0 +1,125 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.webp";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.webp";
+import product9 from "./assets/products/product9.png";
+import product10 from "./assets/products/product10.png";
+import product11 from "./assets/products/product11.png";
+import product12 from "./assets/products/product12.png";
+import product13 from "./assets/products/product13.png";
+import product14 from "./assets/products/product14.png";
+import product15 from "./assets/products/product15.png";
+
+
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Haircut",
+    description: "Haricut of Choice, A full shave and Head Massage",
+    price: 250.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Full Body Massage",
+    description: "A full body massage and spa done by professional women",
+    price: 600.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Kitchen Cleaning",
+    description: "Deep Cleaning of Kitchen Cabinets and Sinks",
+    price: 1099.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Body Waxing",
+    description: "A bikini wax provided for women done at home using prepared wax",
+    price: 2285.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "White Washing",
+    description: "White wash of home done by professional men",
+    price: 4500.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Electrician and Plumber",
+    description: "Skilled workers to setup electrical Appliances. Plumbers to work on faulty materials.",
+    price: 700.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Sofa and Carpet Cleaning",
+    description: "Deep Cleaning and washing of sofa material and carpets of daily use done at factory.",
+    price: 899.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Wall Decor Installation",
+    description: "Installation of wall decor and prints done by professionals. Painting offered too (Charges Apply)",
+    price: 1999.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "TV Installation",
+    description: "Installation of TV, purchased from Girias",
+    price: 599.0,
+    productImage: product9,
+  },
+  {
+    id: 10,
+    productName: "Vegetable Cutting",
+    description: "Cutting of vegetables by professionals, done at home itself.",
+    price: 499.0,
+    productImage: product10,
+  },
+  {
+    id: 11,
+    productName: "Washing Machine Installation",
+    description: "Installing Washing Machine and connecting to a water outlet.",
+    price: 799.0,
+    productImage: product11,
+  },
+  {
+    id: 12,
+    productName: "Yoga Instructor",
+    description: "A trained yoga instructor for a family. Minimum two persons to be involved.",
+    price: 2999.0,
+    productImage: product12,
+  },
+  {
+    id: 13,
+    productName: "Baby Sitting",
+    description: "A well trained baby sitter, capable of doing all tasks required.",
+    price: 4999.0,
+    productImage: product13,
+  },
+  {
+    id: 14,
+    productName: "Geyser and Switch Installation",
+    description: "Installation of Geysers and Switches done by professionals.",
+    price: 699.0,
+    productImage: product14,
+  },
+  {
+    id: 15,
+    productName: "Home Cook",
+    description: "Professional Home Cook capable of preparing meals both Vegetarian and Non Vegetarian (Charges Apply).",
+    price: 2199.0,
+    productImage: product15,
+  },
+];
